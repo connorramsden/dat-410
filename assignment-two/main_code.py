b88f538e70f6b4b9ab4ede616fea1c89b03698ad
Connor Ramsden <connor.ramsden@mymail.champlain.edu>
2020-02-01 12:32:12 -0500

    initial work on ass. 2

--- assignment-two/main_code.py
+++ assignment-two/main_code.py
@@ -2,3 +2,22 @@ import numpy as np
 import pandas as pd
 from tensorflow import keras
 
+
+def read_data(data_file):
+    training_data = pd.read_csv(data_file)
+    pred = 'your code here'
+    return training_data, pred
+
+
+def clean_data(train, pred):
+    id_columns = ['encounter_id', 'icu_id', 'hospital_id', 'patient_id']
+    numerical_data = train.select_dtypes(np.int64)
+    numerical_data = numerical_data.drop(id_columns, axis=1)
+    return numerical_data
+
+
+training, prediction = read_data('data/training_v2.csv')
+
+num_data = clean_data(training, prediction)
+
+print(training.head())
