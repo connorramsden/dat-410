4b2938f296ee8d15d7c64a55c5978a851f08c94f
Connor Ramsden <connor.ramsden@mymail.champlain.edu>
2020-02-04 13:44:50 -0500

    who knows

--- assignment-two/main_code.py
+++ assignment-two/main_code.py
@@ -2,3 +2,15 @@ import numpy as np
 import pandas as pd
 from tensorflow import keras
 
+training = pd.read_csv('data/training_v2.csv', nrows=100)
+
+
+def hospital_model(y_new):
+    model = keras.Sequential(keras.layers.Dense(1))
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    return model.predict(y_new)
+
+
+prediction = hospital_model([7.0])
+print(prediction)
